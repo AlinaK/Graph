5a97780ff282da6fba1c9512b5e7c051abafadba
AlinaK <alina.klauser@googlemail.com>
2011-05-16 10:06:29 +0200

    Zweite Hausaufgabe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graph::Application.config.secret_token = 'ffb490813603b93e89d593025542ce2695a12c1639c0efba98f53eb60eb75857d0beeaaac181ce21464554318eeaaa2301eb8ef4f64dd68ec29b52c6def0d6c1'
